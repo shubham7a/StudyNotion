f988c41f0503234099a5575193f9cb8a5fc0ef20
shubham7a <sj4051688@gmail.com>
2024-08-16 19:55:23 +0530

    First Study Notion

--- src/components/core/Dashboard/Settings/ChangeProfilePicture.jsx
+++ src/components/core/Dashboard/Settings/ChangeProfilePicture.jsx
@@ -43,7 +43,8 @@ export default function ChangeProfilePicture() {
       setLoading(true)
       const formData = new FormData()
       formData.append("displayPicture", imageFile)
-      // console.log("formdata", formData)
+     // console.log("formdata", formData)
+      //console.log("image file", imageFile)
       dispatch(updateDisplayPicture(token, formData)).then(() => {
         setLoading(false)
       })
